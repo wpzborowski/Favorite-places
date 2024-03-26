251f22568d23b904957d6783de311eefcac10947
wpzborowski <wpzborowski@gmail.com>
2024-03-26 15:54:54 +0100

    Final version of the app

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -31,12 +31,12 @@ class _LocationInputState extends State<LocationInput> {
     }
     final latitude = _pickedLocation!.latitude;
     final longitude = _pickedLocation!.longitude;
-    return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=AIzaSyCv6qZLYl4mjF_cMNZ0-LJifOkxyLmKkDc';
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=GOOGLEAPIKEY';
   }
 
   Future<void> _savePlace(double latitude, double longitude) async {
     final url = Uri.parse(
-        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$latitude,$longitude&key=AIzaSyCv6qZLYl4mjF_cMNZ0-LJifOkxyLmKkDc');
+        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$latitude,$longitude&key=GOOGLEAPIKEY');
 
     final response = await http.get(url);
     final resData = json.decode(response.body);
